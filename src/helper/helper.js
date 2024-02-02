d6e6747e26a415a42ffd3446ab7c91f3944c29a8
Your Name <gogemaza14@gmail.com>
2024-02-03 03:16:52 +0400

    bug fix

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -41,39 +41,66 @@ const randomPlaceIndexFunc = (arr) => {
   const num = Math.random() * 100;
   const finalNum = Math.round(num) % indexOfZeros.length;
   const newNumProbability = Math.random() < 0.9 ? 2 : 4;
-  console.log(newNumProbability);
   arr[indexOfZeros[finalNum]] = newNumProbability;
 };
 
 export const rightArrFunc = (arr) => {
+  const startArr = [...arr];
   allTogetherFunc(arr, 0, 1, 2, 3);
   allTogetherFunc(arr, 4, 5, 6, 7);
   allTogetherFunc(arr, 8, 9, 10, 11);
   allTogetherFunc(arr, 12, 13, 14, 15);
-  randomPlaceIndexFunc(arr);
+  const endArr = [...arr];
+  for (let i = 0; i < arr.length; i++) {
+    if (startArr[i] !== endArr[i]) {
+      randomPlaceIndexFunc(arr);
+      break;
+    }
+  }
   return arr;
 };
 export const leftArrFunc = (arr) => {
+  const startArr = [...arr];
   allTogetherFunc(arr, 3, 2, 1, 0);
   allTogetherFunc(arr, 7, 6, 5, 4);
   allTogetherFunc(arr, 11, 10, 9, 8);
   allTogetherFunc(arr, 15, 14, 13, 12);
-  randomPlaceIndexFunc(arr);
+  const endArr = [...arr];
+  for (let i = 0; i < arr.length; i++) {
+    if (startArr[i] !== endArr[i]) {
+      randomPlaceIndexFunc(arr);
+      break;
+    }
+  }
   return arr;
 };
 export const downArrFunc = (arr) => {
+  const startArr = [...arr];
   allTogetherFunc(arr, 3, 7, 11, 15);
   allTogetherFunc(arr, 2, 6, 10, 14);
   allTogetherFunc(arr, 1, 5, 9, 13);
   allTogetherFunc(arr, 0, 4, 8, 12);
-  randomPlaceIndexFunc(arr);
+  const endArr = [...arr];
+  for (let i = 0; i < arr.length; i++) {
+    if (startArr[i] !== endArr[i]) {
+      randomPlaceIndexFunc(arr);
+      break;
+    }
+  }
   return arr;
 };
 export const upArrFunc = (arr) => {
+  const startArr = [...arr];
   allTogetherFunc(arr, 12, 8, 4, 0);
   allTogetherFunc(arr, 13, 9, 5, 1);
   allTogetherFunc(arr, 14, 10, 6, 2);
   allTogetherFunc(arr, 15, 11, 7, 3);
-  randomPlaceIndexFunc(arr);
+  const endArr = [...arr];
+  for (let i = 0; i < arr.length; i++) {
+    if (startArr[i] !== endArr[i]) {
+      randomPlaceIndexFunc(arr);
+      break;
+    }
+  }
   return arr;
 };
