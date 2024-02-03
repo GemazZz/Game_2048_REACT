9f4dfe1b9169058fa7faed02d58fe5d036070527
Your Name <gogemaza14@gmail.com>
2024-02-03 19:13:47 +0400

    logic update

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -122,3 +122,37 @@ export const upArrFunc = (arr) => {
   }
   return arr;
 };
+
+export const isLoseFunc = (arr) => {
+  if (
+    !arr.includes(0) &&
+    arr[0] !== arr[1] &&
+    arr[1] !== arr[2] &&
+    arr[2] !== arr[3] &&
+    arr[0] !== arr[4] &&
+    arr[4] !== arr[5] &&
+    arr[5] !== arr[1] &&
+    arr[5] !== arr[6] &&
+    arr[6] !== arr[2] &&
+    arr[6] !== arr[7] &&
+    arr[7] !== arr[3] &&
+    arr[8] !== arr[4] &&
+    arr[8] !== arr[9] &&
+    arr[8] !== arr[12] &&
+    arr[9] !== arr[5] &&
+    arr[9] !== arr[10] &&
+    arr[9] !== arr[13] &&
+    arr[10] !== arr[6] &&
+    arr[10] !== arr[11] &&
+    arr[10] !== arr[14] &&
+    arr[11] !== arr[7] &&
+    arr[11] !== arr[15] &&
+    arr[12] !== arr[13] &&
+    arr[13] !== arr[14] &&
+    arr[14] !== arr[15]
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
